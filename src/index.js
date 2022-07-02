27037dc870e7666e21de166b3395289f6875d820
MoraJul <akulova13@mail.ru>
2022-07-02 14:29:36 +0300

    feat:task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let result = ""
+let num = Math.abs(n)
+let str = String(num)
+
+  for (i = str.length - 1; i >= 0; i--) {
+    result = `${result}${str[i]}`;
 }
+return result;
+  }
